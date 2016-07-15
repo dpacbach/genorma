555a1d5328537267157e0f74175343a3baa7704d
David P. Sicilia <dpsicilia@gmail.com>
2016-07-15 15:42:27 -0400

    Remove some unused code

--- makerules/printing.mk
+++ makerules/printing.mk
@@ -7,13 +7,6 @@ c_green   := \033[32m
 c_magenta := \033[35m
 c_norm    := \033[00m
 
-# This will colorize the "removed" part of the output of the
-# rm command and will also remove the ticks.  We need to
-# incoke the echo command here because apparently one cannot
-# put escape sequences inside a seq string.
-removed_red    := 's/removed/'`echo -e "$(c_red)"`'  removed'`echo -e "$(c_norm)"`'/'
-remove_ticks   := 's/`//; s/'\''//'
-
 ifneq (undefined, $(origin V))
     at :=
     print_rule :=
