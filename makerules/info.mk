b096b9fa3ad3c95bb22654c35aeb1252c259be8c
David P. Sicilia <unspecified>
2017-11-12 18:29:10 -0500

    Fixing a minor bug

--- makerules/info.mk
+++ makerules/info.mk
@@ -1,6 +1,6 @@
 # This file will display some info about the various settings
 # if the user sets the SHOW_CONFIG variable.
-ifneq ($(origin SHOW_CONFIG),)
+ifneq ($(origin SHOW_CONFIG),undefined)
 
 gxx-version := $(shell $(CXX) -dumpfullversion)
 $(info $(call output_using,g++ $(gxx-version)))
